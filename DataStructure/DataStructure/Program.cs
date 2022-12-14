a3a98a4089356b070f4498fc9a8515b019ef51ab
Amoljamnik97 <ajamnik.mech@gmail.com>
2022-12-14 10:54:52 +0530

    [amol]- Stack and Dequeue

--- DataStructure/DataStructure/Program.cs
+++ DataStructure/DataStructure/Program.cs
@@ -19,12 +19,47 @@ namespace DataStructure
 
 
 
-            list.Addreverse(10);
-            list.Addreverse(20);
-            list.Addreverse(30);
-            list.Display();
-            list.Delete();
+            //list.Addreverse(10);
+            //list.Addreverse(20);
+            //list.Addreverse(30);
+            //list.Display();
+            //list.Delete();
             //list.DeleteLast();
+
+
+
+            //stack_Ex stack_ = new stack_Ex();
+            //stack_.push(10);
+            //stack_.push(20);
+            //stack_.push(30);
+            //stack_.peek();
+            //stack_.Display();
+
+            //stack_.Pop();
+
+            //stack_.Display();
+            //stack_.Pop();
+
+            //stack_.Display();
+            //stack_.Pop();
+
+            //stack_.Display();
+            //stack_.Pop();
+            //
+
+            Queue_Ex queue_Ex = new Queue_Ex();
+            queue_Ex.Enqueue(10);
+            queue_Ex.Enqueue(20);
+            queue_Ex.Enqueue(30);
+            queue_Ex.Enqueue(40);
+            queue_Ex.Display();
+
+            queue_Ex.Dequeue();
+            queue_Ex.Display();
+            queue_Ex.Dequeue();
+            queue_Ex.Display();
+            queue_Ex.Dequeue();
+            queue_Ex.Display();
         }
     }
 }
---
+++ DataStructure/DataStructure/stack_Ex.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class stack_Ex
+    {
+        //top--push--insert, pop---delete,peek--it will returen top element
+        Node top;
+        public stack_Ex()
+        {
+            this.top = null;
+        }
+        public void push(int data)
+        {
+            Node node = new Node(data);
+            if (top == null)
+            {
+                node.Next = null;
+            }
+            else
+            {
+                node.Next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack",data);
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data+"");
+                temp = temp.Next;
+            }
+        }
+        public void peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            else
+            {
+                Console.WriteLine("{0} is in the top of stack",top.data);
+            }
+        }
+        public void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty deletion is not possible");
+            }
+            else
+            {
+                Console.WriteLine("Values popped is {0}",top.data);
+                top = top.Next;
+            }
+        }
+    }
+}
